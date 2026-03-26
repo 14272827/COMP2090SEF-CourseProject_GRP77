2cf7b3607c121d59396eefbc69fbdbb5adbee283
Yukineflm <cyuki5755@gmail.com>
2026-03-27 01:48:18 +0800

    Update GUI.py

--- Task1/GUI.py
+++ Task1/GUI.py
@@ -12,6 +12,11 @@ class LoginGUI:
         self.create_users()
         self.setup()
 
+    # default testing acc(admin)
+    def create_users(self):
+        admin = Admin()
+        admin.register_user("admin123", "654321", "admin")
+
     def setup(self):
         title = tk.Label(self.window, text="Library System", font=self.title_font)
         title.pack(pady=10)
